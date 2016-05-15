0a3a7ac7ce48f410d3e70c1a105ec5435650631c
gabriel souza de paula <gabriel souza de paula>
2016-05-15 15:41:28 -0300

    Icone

--- JavaStock-dev/src/projeto/javastock/login/view/LoginView.java
+++ JavaStock-dev/src/projeto/javastock/login/view/LoginView.java
@@ -1,17 +1,12 @@
 package javastock.login.view;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 public class LoginView extends Application {
 
@@ -26,10 +21,10 @@ public class LoginView extends Application {
         );
 
         primaryStage.setScene(new Scene(root));
-
-        primaryStage.setTitle("Login JavaStock");
+        primaryStage.getIcons().add(new Image("sacola.png"));
+        primaryStage.setTitle("JavaStock Comercial Plataform");
         primaryStage.show();
-        Image image = new Image("javastock/login/view/img/LogoJavaStock.png");
+        Image image = new Image("sacola.png");
         ImageView imageView = new ImageView(image);
         imageView.setVisible(true);
 

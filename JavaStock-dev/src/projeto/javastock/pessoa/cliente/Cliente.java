77dfa29bf5ca6308cf47a4a9e0be02d5ca17097f
gabriel souza de paula <gabriel souza de paula>
2016-06-17 23:59:40 -0300

    #9 Falta fazer busca por ID de cliente

--- JavaStock-dev/src/projeto/javastock/pessoa/cliente/Cliente.java
+++ JavaStock-dev/src/projeto/javastock/pessoa/cliente/Cliente.java
@@ -10,6 +10,23 @@ import javastock.pessoa.Pessoa;
 public class Cliente extends Pessoa implements Entidade {
 
 
+    /**
+     * Cria uma nova pessoa.
+     *
+     * @param nome     Nome da pessoa.
+     * @param cpf      CPF da pessoa.
+     * @param rg       Numero da carteira de identidade da pessoa.
+     * @param email    Email da pessoa.
+     * @param endereco Endereco da pessoa.
+     * @param status   Status da pessoa.
+     * @throws RuntimeException Caso algum argumento nao passe pela validacao.
+     */
+    public Cliente(String nome, String cpf, String rg, String email, Endereco endereco, int status) {
+        super(nome, cpf, rg, email, endereco, status);
+    }
+
+    
+
 
     /**
      * Cria uma nova pessoa.
@@ -19,9 +36,12 @@ public class Cliente extends Pessoa implements Entidade {
      * @param rg       Numero da carteira de identidade da pessoa.
      * @param email    Email da pessoa.
      * @param endereco Endereco da pessoa.
+     * @param status   Status da pessoa.
      * @throws RuntimeException Caso algum argumento nao passe pela validacao.
      */
-    public Cliente(String nome, String cpf, String rg, String email, Endereco endereco) {
-        super(nome, cpf, rg, email, endereco);
+    public Cliente(int idCliente, String nome, String cpf, String rg, String email, Endereco endereco, int status) {
+        super(nome, cpf, rg, email, endereco, status);
+        this.setIdPessoa(idCliente);
     }
+
 }
---
+++ JavaStock-dev/src/projeto/javastock/pessoa/cliente/ClienteDAO.java
@@ -0,0 +1,101 @@
+package javastock.pessoa.cliente;
+
+import javastock.misc.DAO;
+import javastock.misc.DatabaseFactory;
+import javastock.pessoa.PessoaDAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ * DAO de {@link Cliente}.
+ * Created by gabri on 17/06/2016.
+ */
+public class ClienteDAO implements DAO<Cliente> {
+    public static final ClienteDAO instancia = new ClienteDAO();
+
+    /**
+     * Construtor privado para não instanciar ClienteDAO.
+     */
+    private ClienteDAO() {
+
+    }
+
+    /**
+     * Retorna instancia de clienteDAO.
+     * @return
+     */
+    public static ClienteDAO getInstancia() {
+        return instancia;
+    }
+
+    /**
+     * Método para salvar Cliente.
+     * @param cliente
+     * @return
+     */
+    @Override
+    public int salvar(Cliente cliente) {
+        try (Connection connection = this.getConnection()) {
+            if (cliente.getIdPessoa() == -1)
+                return this.criar(connection, cliente);
+            else
+                return this.atualizar(connection, cliente);
+        } catch (SQLException e) {
+            System.out.print(e);
+        }
+
+        return -1;
+    }
+
+    @Override
+    public List<Cliente> listar() {
+        return null;
+    }
+
+    /**
+     * Metodo para conectar na base de dados.
+     * @return
+     * @throws SQLException
+     */
+    private Connection getConnection() throws SQLException {
+        return new DatabaseFactory().getConnection();
+    }
+
+    /**
+     * Criar cliente no banco.
+     * @param connection
+     * @param cliente
+     * @return
+     * @throws SQLException
+     */
+    public int criar(Connection connection, Cliente cliente) throws SQLException {
+        int idPessoa = PessoaDAO.getInstancia().salvar(cliente);
+
+        String sql = "INSERT INTO Cliente (c_id_pessoa) VALUES (?)";
+
+        PreparedStatement stmt = connection.prepareStatement(sql);
+        stmt.setInt(1, idPessoa);
+
+        stmt.execute();
+        stmt.close();
+
+        return idPessoa;
+
+    }
+
+    /**
+     * Método para atualizar cliente.
+     * @param connection
+     * @param cliente
+     * @return
+     */
+    public int atualizar(Connection connection, Cliente cliente) {
+        int idPessoa = PessoaDAO.getInstancia().salvar(cliente);
+
+
+        return idPessoa;
+    }
+}

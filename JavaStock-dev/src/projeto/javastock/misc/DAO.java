33ea3d59ffae3ca2170b34210e585fccc6fba4ef
fernandobd42 <fernandolucasbd@gmail.com>
2016-06-19 17:40:17 -0300

    Correcao DAO

--- JavaStock-dev/src/projeto/javastock/misc/DAO.java
+++ JavaStock-dev/src/projeto/javastock/misc/DAO.java
@@ -9,6 +9,7 @@ public interface DAO<T extends Entidade> {
     /**
      * Salva entidade no banco dados. Caso possua um ID, atualiza tupla,
      * caso contraria uma nova.
+     *
      * @param entidade Registro a ser salvo.
      * @return Id da entidade salva.
      */
@@ -16,19 +17,17 @@ public interface DAO<T extends Entidade> {
 
     /**
      * Recupera todos os registros da banco.
+     *
      * @return Lista de registros encontrados.
      */
     List<T> listar();
 
-<<<<<<< HEAD
-
-=======
     /**
      * Busca entidade que tem a PK informada.
+     *
      * @param id PK do registro.
      * @return Entidade encontrada.
      */
     T getById(int id);
->>>>>>> origin/master
 }
 
